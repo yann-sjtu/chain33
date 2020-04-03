9f47f21dd2fe67729d0ecd7fb08909abc0ffa75b
yxq <yxq@disanbo.com>
2020-04-03 15:56:20 +0800

    use proto message

--- p2pnext/types/message.go
+++ p2pnext/types/message.go
@@ -5,33 +5,13 @@ import (
 	"time"
 )
 
-//Message 协议传递时统一消息类型
-// Protocol指定具体协议
-// Data是消息体
-type Message struct {
-	ProtocolID string
-	Params     interface{}
-}
-
-type Response struct {
-	Result interface{}
-	Error  error
-}
-
-type StorageData struct {
-	Data        interface{}
-	RefreshTime time.Time
-}
-
 var (
 	ErrLength             = errors.New("length not equal")
 	ErrProtocolNotSupport = errors.New("protocol not support")
 	ErrNotFound           = errors.New("not found")
 	ErrInvalidParam       = errors.New("invalid parameter")
+	ErrInvalidMessage     = errors.New("invalid message")
 	ErrInvalidResponse    = errors.New("invalid response")
-	ErrUnexpected         = errors.New("unexpected error")
-	ErrDataExpired        = errors.New("data expired")
-	ErrDBSave             = errors.New("save data to DB error")
 
 	ExpiredTime     = time.Hour * 24
 	RefreshInterval = time.Hour * 4
