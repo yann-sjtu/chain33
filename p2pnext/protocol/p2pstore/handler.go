a45e0ef6af8b83c01fa2b3d8275e1604fa10524d
yxq <yxq@disanbo.com>
2020-04-07 11:02:15 +0800

    move rw

--- p2pnext/protocol/p2pstore/handler.go
+++ p2pnext/protocol/p2pstore/handler.go
@@ -219,3 +219,28 @@ func (s *StoreProtocol) onGetChunkRecord(writer *bufio.Writer, req *types.ReqChu
 	}
 	res.Result = &types.P2PStoreResponse_ChunkRecords{ChunkRecords: resp.GetData().(*types.ChunkRecords)}
 }
+
+func readMessage(reader *bufio.Reader, msg types.Message) error {
+	var data []byte
+	for {
+		buf := make([]byte, 1024)
+		n, err := reader.Read(buf)
+		if err != nil {
+			return err
+		}
+		data = append(data, buf[:n]...)
+		if n < 1024 {
+			break
+		}
+	}
+	return types.Decode(data, msg)
+}
+
+func writeMessage(writer *bufio.Writer, msg types.Message) error {
+	b := types.Encode(msg)
+	_, err := writer.Write(b)
+	if err != nil {
+		return err
+	}
+	return writer.Flush()
+}
